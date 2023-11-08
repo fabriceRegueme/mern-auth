e46263b594cad43c99733a35e9eea87a106323ef
fabriceRegueme <fabrice.regueme@gmail.com>
2023-11-08 09:14:34 +0100

    complete user update

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,45 @@
-import {useSelector} from 'react-redux';
+import {useSelector,useDispatch} from 'react-redux';
 import { useRef,useState,useEffect } from 'react';
 import {getDownloadURL, getStorage,ref, uploadBytesResumable} from 'firebase/storage';
 import {app} from '../firebase';
+//import e from 'express';
+import {updateUserStart,updateUserSuccess,updateUserFailure} from '../redux/user/userSlice.js'
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const {currentUser} = useSelector(state => state.user);
+  const {currentUser,error,loading} = useSelector(state => state.user);
   const [image,setImage] = useState(undefined);
   const [imagePercent, setImagePercent] = useState(0);
   const [imageError, setImageError] = useState(false);
   const [formData, SetFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
+
+  
+
+  const handleChange = (e) => {
+    SetFormData({...formData, [e.target.id]: e.target.value})
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {method: 'POST', headers:  {'Content-Type': 'application/json'},body: JSON.stringify(formData)});
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data));
+        setUpdateSuccess(false);
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+       
+    } catch (error) { 
+      dispatch(updateUserFailure(error));
+      setUpdateSuccess(false);
+    }
+  }
 
   useEffect(() => {
     if (image) {
@@ -22,6 +52,7 @@ export default function Profile() {
     const fileName =  new Date().getTime() + image.name;
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef,image);
+    
     uploadTask.on(
       'state_changed',
       (snapshot) => {
@@ -39,7 +70,7 @@ export default function Profile() {
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold my-7 text-center'>Profile</h1>
-      <form className='flex flex-col gap-4'> 
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'> 
         <input type='file' hidden ref={fileRef} accept='image/*' onChange={(e) => setImage(e.target.files[0])}/>
         <img 
         src={formData.profilePicture || currentUser.profilePicture}   
@@ -52,19 +83,21 @@ export default function Profile() {
           (<span className='text-red-700'>Erreur de téléchargement</span>) 
           : 
           imagePercent > 0 && imagePercent < 100 ?
-          (<span className='text-slate-700'>{`Téléchargement en cours ... + {imagePercent} %`}</span>)
+          (<span className='text-slate-700'>{`Téléchargement en cours ... + ${imagePercent} %`}</span>)
           : imagePercent === 100 ?
-          (<span className='text-green-700'>Image chargée</span>) : ('')}
+          (<span className='text-green-700'>Image chargée</span>) : ('')} 
         </p>
-        <input type='text' id='username' placeholder='Username' className='bg-slate-200 rounded-lg p-3' defaultValue={currentUser.username} />
-        <input type='text' id='email' placeholder='E-mail' className='bg-slate-200 rounded-lg p-3' defaultValue={currentUser.email}/>
-        <input type='password' id='password' placeholder='Password' className='bg-slate-200 rounded-lg p-3' />
-        <button className='mt-5 p-3 text-white bg-slate-700 font-semibold uppercase rounded-lg hover:opacity-95 disabled: opacity-80'>update</button>
+        <input type='text' id='username' placeholder='Username' className='bg-slate-200 rounded-lg p-3' defaultValue={currentUser.username} onChange={handleChange} />
+        <input type='text' id='email' placeholder='E-mail' className='bg-slate-200 rounded-lg p-3' defaultValue={currentUser.email} onChange={handleChange} />
+        <input type='password' id='password' placeholder='Password' className='bg-slate-200 rounded-lg p-3' onChange={handleChange} />
+        <button className='mt-5 p-3 text-white bg-slate-700 font-semibold uppercase rounded-lg hover:opacity-95 disabled: opacity-80'>{loading ? 'Chargement ...' : 'Mis à jour'}</button>
       </form>
       <div className='flex justify-between mt-2'>
         <span className='text-red-700 cursor-pointer font-semibold'>Delete account</span>
         <span className='text-red-700 cursor-pointer font-semibold'>Sign Out</span>
       </div>
+      <p className='text-red-700 mt-5'>{error && "Problème rencontré"}</p>
+      <p className='text-green-700 mt-5'>{updateSuccess && "Profile mis à jour."}</p>
     </div>
   )
 }
